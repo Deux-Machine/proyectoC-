6b378d6f0b2e918a8bb065ce9cbb3b282aa92a28
Sebastian Alvarez <alvarezsebas552@gmail.com>
2023-02-18 19:05:17 -0500

    primer Commit

---
+++ Etapa1/Entidades/Escuela.cs
@@ -0,0 +1,10 @@
+namespace CoreEscuela.Entidades
+{
+    class Escuela{
+        string Nombre;
+        public string Nombre {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+    }
+}
